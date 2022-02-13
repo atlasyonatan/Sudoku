11344e21db4398d40ec23378bd974752f2a0b309
Yonatan Atlas <atlasyonatan@gmail.com>
2022-02-13 16:08:55 +0200

    added hashsetsolver

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = @"D:\Repos\Sudoku\SudokuSolver\Puzzles\SudokuEasy1.txt";
+            var puzzleString = File.ReadAllText(filePath);
+            var puzzle = Serialize.FromBoardString(puzzleString);
+            Console.WriteLine("Puzzle:");
+            Console.WriteLine(Serialize.ToBoardString(puzzle));
+            Console.WriteLine();
+            var solutions = SolverHashSet.Solve(puzzle);
+            foreach (var solution in solutions)
+            {
+                Console.WriteLine(Serialize.ToBoardString(solution));
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ SudokuSolver/Serialize.cs
@@ -0,0 +1,32 @@
+﻿using Sudoku;
+using System;
+using System.Linq;
+
+namespace SudokuSolver
+{
+    public static class Serialize
+    {
+        public static Cell[,] FromBoardString(string puzzle)
+        {
+            puzzle = puzzle.Replace(" ", "").Replace("|", "").Replace("-" + Environment.NewLine, "").Replace("-", "");
+            var grid = puzzle.Split(Environment.NewLine).Select(s => s.ToArray()).ToArray();
+            var board = new Cell[9, 9];
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    board[i, j] = grid[j][i] switch
+                    {
+                        '.' => Cell.Empty,
+                        _ => (Cell)(grid[j][i] - '0')
+                    };
+            return board;
+        }
+
+        public static string ToBoardString(Cell[,] board) =>
+            string.Join('\n', Enumerable.Range(0, board.GetLength(1)).Select(j => 
+                (j % 3 == 0 && j > 0 ? "-------|-------|-------\n" : "")
+                + string.Join("", Enumerable.Range(0, board.GetLength(0)).Select(i => 
+                    (i % 3 == 0 && i > 0 ? " |" : "") 
+                    + " " + board[i, j] switch { Cell.Empty => '.', _ => (char)(board[i, j] + '0') }))));
+
+    }
+}
